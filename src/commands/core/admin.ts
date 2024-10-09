2174a69738b1e6ce67aa5c746f5adb2fec46e4ce
OCbwoy3 <ocbwoy3@ocbwoy3.dev>
2024-10-09 09:32:28 +0300

    a

--- src/commands/core/admin.ts
+++ src/commands/core/admin.ts
@@ -194,7 +194,8 @@ module.exports = {
 				switch (subcommand) {
 					case "bypass_identity": {
 						const new_handle = interaction.options.get('new_handle')?.value as string
-						const user: string = (interaction.options.get('user')?.value || interaction.user.id) as string;
+						const uo = interaction.options.get('user')
+						const user: string = (uo && uo.value || interaction.user.id) as string;
 						const ud = await getUserInfo(user)
 
 						prisma.user.update({
